93b56e6ca7e400a9e1c3a2562388dac7d5d1e09c
mashida <mashidatk1999@gmail.com>
2024-09-09 19:41:27 +0530

    ignore3

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const path = require('path');
 let env = require("dotenv").config(); 
 
 
-// Parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Parse application/json
 app.use(bodyParser.json());
 
 const userRoute= require("./routes/userRouter")
@@ -23,10 +22,10 @@ connectDB();
 app.use('/', express.static(path.join(__dirname, 'public')))    
 app.use(bodyParser.json({limit:"1200kb"}))
 const corsOptions = {
-    origin:[process.env.CORS_API] , // allow only this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // allowed methods
-    credentials: true, // enable set cookie
-    optionsSuccessStatus: 204 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin:[process.env.CORS_API] , 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true, 
+    optionsSuccessStatus: 204 
   };
   
   app.use(cors(corsOptions));
